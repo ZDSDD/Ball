0dbfc9bb1a73a9c571dcd34809926fe222ee547d
ZDSDD <sebasadowy@gmail.com>
2023-11-16 23:21:29 +0100

    add prefab, separate scripts for checkpoints and finish. Optimize follow camera. Add physic material to walls

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,15 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoints : MonoBehaviour
+public class Finish : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //todo: Implement logic to end the level. UI stuff
         if(other.CompareTag("Player"))
         {
             PlayerController playerController = other.GetComponent<PlayerController>();
-            playerController.UpdateCheckpoint(new Vector2(this.transform.position.x, this.transform.position.y));
+            Debug.Log("BRAVO, YOU FINISHED!");
+            playerController.Reset();
         }
     }
 }
