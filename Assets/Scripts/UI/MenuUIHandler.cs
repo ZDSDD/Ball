b33d79798c536aa7026239a88eb10a248d12cc63
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2023-11-27 15:57:09 +0100

    add level select scene

--- Assets/Scripts/UI/MenuUIHandler.cs
+++ Assets/Scripts/UI/MenuUIHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 //make sure this scripts executes after all important data is setup
 [DefaultExecutionOrder(1000)]
@@ -13,5 +14,11 @@ public class MenuUIHandler : MonoBehaviour
         EditorApplication.ExitPlaymode();
 #endif
         Application.Quit();
+    }
+
+    public void SelectLevel()
+    {
+        // 1 should be default for level scene
+        SceneManager.LoadScene(1);
     }
 }
