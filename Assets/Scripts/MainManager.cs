4950ad7ea34abd95f9a553f49193a758479c3e02
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2023-12-11 22:03:58 +0100

    add save progress logic

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,15 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
     public static MainManager Instance;
+    
+    private HashSet<int> _unlockedLevels;
 
-    private List<int> _unlockedLevels;
+    public HashSet<int> UnlockedLevels => _unlockedLevels;
 
+    // MainManager is a singleton
     private void Awake()
     {
         if (Instance is not null)
@@ -24,14 +29,14 @@ public class MainManager : MonoBehaviour
     [Serializable]
     class SaveData
     {
-        public List<int> unlockedLevels = new List<int>();
+        public List<int> unlockedLevels;
     }
 
     public void SaveProgress(int level)
     {
         SaveData data = new SaveData();
-        
-        data.unlockedLevels.Add(level);
+        _unlockedLevels.Add(level);
+        data.unlockedLevels = new List<int>(_unlockedLevels);
 
         string json = JsonUtility.ToJson(data);
         File.WriteAllText(Application.persistentDataPath + "/savefile.json",json);
@@ -39,13 +44,14 @@ public class MainManager : MonoBehaviour
 
     public void LoadProgress()
     {
+        _unlockedLevels = new HashSet<int>(){0};
         string path = Application.persistentDataPath + "/savefile.json";
         if (File.Exists(path))
         {
             string json = File.ReadAllText(path);
             SaveData data = JsonUtility.FromJson<SaveData>(json);
-
-            _unlockedLevels = data.unlockedLevels;
+            _unlockedLevels.AddRange(data.unlockedLevels);
         }
     }
+
 }
