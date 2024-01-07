49ba807b24104611076ea76952cf06602ea43040
ZDSDD <106777224+ZDSDD@users.noreply.github.com>
2024-01-07 22:55:59 +0100

    deleted out of date 'todo' comment

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -9,7 +9,6 @@ public class Finish : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //todo: Implement logic to end the level. UI stuff
         if(other.CompareTag("Player"))
         {
             PlayerController playerController = other.GetComponent<PlayerController>();
